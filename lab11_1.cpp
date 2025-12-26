a340884c89d6589ef6a5ded8bb28a011ab2d97fd
Korrawit <greater2316@gmail.com>
2025-12-26 17:08:22 +0700

    yes

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,26 @@
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+srand(time(0));
+int x;
+cout << "Press Enter 3 times to reveal your future.";
+for(int i = 0 ; i<=2 ; i++){
+    cin.get();
+}
+x = rand() % 9;
+if(x == 0) cout << "You will get A in this 261102." ;
+else if(x == 1) cout << "You will get B+ in this 261102." ;
+else if(x == 2) cout << "You will get B in this 261102." ;
+else if(x == 3) cout << "You will get C+ in this 261102." ;
+else if(x == 4) cout << "You will get C in this 261102." ;
+else if(x == 5) cout << "You will get D+ in this 261102." ;
+else if(x == 6) cout << "You will get D in this 261102." ;
+else if(x == 7) cout << "You will get F in this 261102." ;
+else if(x == 8) cout << "You will get W in this 261102." ;
+}
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
